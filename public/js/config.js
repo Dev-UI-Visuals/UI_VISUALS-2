06f45f60232e5f324c8394a37b2bd6c660a19b93
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-07-13 21:25:27 +0545

    Working on Why Section

--- public/js/config.js
+++ public/js/config.js
@@ -20,28 +20,28 @@ tailwind.config = {
         'text-main': "#3a3a3c",
         'text-mid': "#585858",
         'text-light': '#6b7070',
-        'nav-main':'rgb(250, 250, 250,1)',
-        'nav-light':'rgb(250, 250, 250,0.7)',
+        'nav-main': 'rgb(250, 250, 250,1)',
+        'nav-light': 'rgb(250, 250, 250,0.7)',
       },
       fontSize: {
-        xxxxl:"60px",
-        xxxl:"50px",
-        xxl:'40px',
+        xxxxl: "60px",
+        xxxl: "50px",
+        xxl: '40px',
         xl: '35px',
         lg: "30px",
         md: "22px",
-        rg:"20px",
+        rg: "20px",
         sm: "18px",
         xs: "16px",
         xxs: "14px",
-        xxxs:"12px"
+        xxxs: "12px"
       },
-      fontWeight:{
-        b:'800',
-        sb:'700',
-        md:'600',
-        r:'500',
-        l:'400'
+      fontWeight: {
+        b: '800',
+        sb: '700',
+        md: '600',
+        r: '500',
+        l: '400'
       }
 
     },
