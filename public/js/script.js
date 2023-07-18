d3708caa892a073406db479746ba9edd13c15eeb
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-07-18 13:20:06 +0545

    Updated Some Events Data

--- public/js/script.js
+++ public/js/script.js
@@ -6,33 +6,92 @@ var moreEvents = [
     {
         "id": "herald-code-brisk-hackathon",
         "name": "Herald Code Brisk Hackathon",
-        "type": "Major Event",
         "location": "Kuri Village, Kalinchowk",
         "date": "2023-04-23",
-        "headerImage": "https://heraldcollege.edu.np/storage/post/HCK-Code-Brisk-post_1676978748.jpg",
-        "profileImage": "/image/events/Code_Brisk.png",
+        "headerImage": [
+            "https://heraldcollege.edu.np/storage/post/HCK-Code-Brisk-post_1676978748.jpg",
+            "/image/events/Code_Brisk/Code_Brisk_1.png",
+            "https://heraldcollege.edu.np/storage/uploads/Herald%20Code%20Brisk_1676978644.jpg",
+            "https://heraldcollege.edu.np/storage/uploads/Herald%20Code%20Brisk%20Winner_1676978715.jpg"
+        ],
+        "profileImage": "/image/events/Code_Brisk/Code_Brisk.png",
         "description": [
-            "Herald DevCorps UI Visual Community, a student-led community at Herald College Kathmandu, successfully organized the nonstop 24-hour 'Code Brisk Hackathon 2023', at Kuri Village Kalinchowk. Situated at an altitude of 3447m above sea level, Code Brisk marks the hackathon held at the highest altitude. A total of 13 colleges participated in the hackathon, with three outstanding teams chosen to construct their prototypes at Kalinchowk. The primary theme of this competition was to boost the tourism and hospitality sector of Kalinchowk through innovative and practical prototypes created by the top three teams.",
-            "'Our community's unwavering effort and dedication to this project is commendable and has resulted in an incredible outcome. It is a testament to the passion and talent within our community, and I am proud of every one of you', says Mr. Prakash Shrestha, the CEO of Herald College Kathmandu, expressing his gratitude towards the organizing team.Herald College Kathmandu congratulates the winning team, Virinchi College - Team Everest for their revolutionary project, 'Tidy Town', which employs sensors to tackle waste management issues and offers a practical way of waste disposal. We would also like to extend our appreciation to all the participating colleges. Ms. Sujata Thapa, Head of Herald Devcorps, also praised the organizing committee and the top three colleges, “The effort of everyone in our community is reflected in the incredible work accomplished, and I am honored to be a part of it. Additionally, the top 3 teams presented truly impressive and achievable ideas, leaving us amazed."
+            {
+                "paragraph": "Herald DevCorps UI Visual Community, a student-led community at Herald College Kathmandu, successfully organized the nonstop 24-hour 'Code Brisk Hackathon 2023', at Kuri Village Kalinchowk. Situated at an altitude of 3447m above sea level, Code Brisk marks the hackathon held at the highest altitude.",
+                "image": null
+            },
+            {
+                "paragraph": "A total of 13 colleges participated in the hackathon, with three outstanding teams chosen to construct their prototypes at Kalinchowk. The primary theme of this competition was to boost the tourism and hospitality sector of Kalinchowk through innovative and practical prototypes created by the top three teams.",
+                "image": null
+            },
+            {
+                "paragraph": " Our community's unwavering effort and dedication to this project is commendable and has resulted in an incredible outcome. It is a testament to the passion and talent within our community, and I am proud of every one of you, says Mr. Prakash Shrestha, the CEO of Herald College Kathmandu, expressing his gratitude towards the organizing team.",
+                "image": null
+            },
+            {
+                "paragraph": "Ms. Sujata Thapa, Head of Herald Devcorps, also praised the organizing committee and the top three colleges, “The effort of everyone in our community is reflected in the incredible work accomplished, and I am honored to be a part of it. Additionally, the top 3 teams presented truly impressive and achievable ideas, leaving us amazed.",
+                "image": null
+            },
+            {
+                "paragraph": "Herald College Kathmandu congratulates the winning team, Virinchi College - Team Everest for their revolutionary project, 'Tidy Town', which employs sensors to tackle waste management issues and offers a practical way of waste disposal. We would also like to extend our appreciation to all the participating colleges.",
+                "image": null
+            },
+            {
+                "paragraph": null,
+                "image": "/image/events/Code_Brisk/Code_Brisk_1.png"
+            },
+            {
+                "paragraph": "Furthermore, the ideas of the runner-up teams were: Amrit Science Campus (ASCOL): 'Prawas', a mobile app that facilitates transport and lodging options for the visitors of Kuri Village.",
+                "image": null
+            },
+            {
+                "paragraph": null,
+                "image": "/image/events/Code_Brisk/Code_Brisk_2.png"
+            },
+            {
+                "paragraph": "The British College: 'Snowhub', an online platform that uses customer feedback to suggest optimal hotels and implements intelligent pricing strategies..",
+                "image": null
+            },
+            {
+                "paragraph": null,
+                "image": "/image/events/Code_Brisk/Code_Brisk_3.png"
+            },
+            {
+                "paragraph": "The Code Brisk Hackathon 2023 was a resounding success and demonstrated the exceptional creativity and technical skill of the students in Nepal.",
+                "image": null
+            }
         ],
         "youtubeCode": "50aIeMJ5xyQ",
         "images": [
-            "/image/events/Code_Brisk_1.png",
-            "/image/events/Code_Brisk_1.png",
-            "/image/events/Code_Brisk_1.png",
-            "/image/events/Code_Brisk_1.png"
+            "/image/events/Code_Brisk/Code_Brisk_3.png"
         ]
     },
     {
         "id": "upc-design-hunt",
         "name": "UPC Design Hunt",
-        "type": "Minor Event",
         "location": "Herald College, Naxal",
         "date": "2023-02-09",
-        "headerImage": "/image/events/UPC_Design_Hunt_1.png",
-        "profileImage": "/image/events/UPC_Design_Hunt.png",
+        "headerImage": [
+            "/image/events/UPC_Design_Hunt/UPC_Design_Hunt_1.png",
+            "https://lh3.googleusercontent.com/chat_attachment/AOFd8YYHlV91B1_YAXr-0AhsrzdIbFfQdnLI8u0aAsByCw-pgUW1X2fbPIxml_5ps-Az29YT3-QXl4Rm4tKw3ApkufiVgQvYNh_Xp6GxRgiE6YiyvHaRNSv5bkGODDvGNiorndPDS3oMKZbVPkrkTNTmzv6EgdRp10exeipw4MruayGn5gpAY2d3QQ7cF_X1ELxbEX5fI4xOLgu0jihU4FizvMWCogRVMj6Ngga4-j69IgCb41rLHDcah8bmD9-Z4zJ0AQYIbEHt18ZcKSle2ZJC4LM0guQXyMvIBIsVqyiBbhHwFkcQ5g7_tF0Ex05lWw=w512"
+        ],
+        "profileImage": "/image/events/UPC_Design_Hunt/UPC_Design_Hunt.png",
         "description": [
-            "UI Visuals organized an exciting event called Design Hunt, aimed at providing a platform for students to showcase their creativity and design skills. The event focused on designing a mobile app with unique features for new UPC early birds. The event was executed successfully, thanks to the efforts and support of our members. The event aimed to challenge participants' creativity and problem-solving skills while promoting collaboration and teamwork. Participants were given the resources to design a mobile app with unique features for new UPC early birds, showcasing their skills and potential in the UI and Visual Design field. The event encouraged participants to explore their creativity and learn from their peers and community leaders."
+            {
+                "paragraph":"UI Visuals recently organized an exciting event called Design Hunt, aimed at providing a platform for students to showcase their creativity and design skills. The event focused on designing a mobile app with unique features for new UPC early birds. The event was executed successfully, thanks to the efforts and support of our members.",
+                "image":null    
+            },
+            {
+                "paragraph":"The event was structured to challenge participants' creativity and problem-solving abilities while encouraging collaboration and teamwork. Participants were provided with the necessary materials and resources to design their mobile app on paper, and they had to come up with unique and innovative features that would attract and engage new UPC early birds. The Design Hunt event was an excellent opportunity for students to showcase their skills and potential in the field of UI and Visual Design. It allowed them to explore their creativity and come up with new and innovative ideas while learning from their peers and community leaders.",
+                "image":null
+            },{
+                "paragraph":null,
+                "image":"/image/events/UPC_Design_Hunt/UPC_Design_Hunt_3.png"
+            },
+            {
+                "paragraph":"Overall, the event was a huge success, and we are proud of our members for their dedication and hard work in organizing and executing the event. We look forward to organizing more such events in the future and providing students with opportunities to showcase their skills and creativity in the field of UI and Visual Design.",
+                "image":null
+            }
         ],
         "youtubeCode": null,
         "images": []
@@ -40,13 +99,30 @@ var moreEvents = [
     {
         "id": "vision-quest",
         "name": "Vision Quest",
-        "type": "Minor Event",
         "location": "Herald College, Naxal",
         "date": "2022-09-01",
-        "headerImage": "/image/events/Vision_Quest_1.png",
-        "profileImage": "/image/events/Vision_Quest.png",
+        "headerImage": [
+            "/image/events/Vision_Quest/Vision_Quest_3.png",
+            "/image/events/Vision_Quest/Vision_Quest_1.png"
+        ],
+        "profileImage": "/image/events/Vision_Quest/Vision_Quest.png",
         "description": [
-            "UI Visuals organized a remarkable event to help +2 students interested in the field of IT. The event named Vision Quest: Sneak Peek into the Design World was organized to give aspiring software developers and designers a first-hand experience of the design process. The event was specially designed for +2 students who were looking forward to pursuing a career in the field of IT. The event focused on two key topics, namely 'Software Development' and 'UI/UX Design'. Both of these topics are critical to the IT industry, and understanding them is essential for anyone looking to make a career in the field. The event was curated by the community leaders who had years of experience in these fields and had a lot to share with the participants. The event was a great success, with many students attending and learning a lot about software development and UI/UX design. The participants were enthusiastic, and the experts' presentations were engaging and informative. Overall, the event was an excellent opportunity for +2 students to learn about the IT industry and gain valuable insights into software development and UI/UX design."
+           {
+            "paragraph":"UI Visuals organized a remarkable event to help +2 students interested in the field of IT. The event named Vision Quest: Sneak Peek into the Design World was organized to give aspiring software developers and designers a first-hand experience of the design process. The event was specially designed for +2 students who were looking forward to pursuing a career in the field of IT.",
+            "image":null
+           },
+           {
+            "paragraph":"The event focused on two key topics, namely 'Software Development' and 'UI/UX Design'. Both of these topics are critical to the IT industry, and understanding them is essential for anyone looking to make a career in the field. The event was curated by the community leaders who had years of experience in these fields and had a lot to share with the participants.",
+            "image":null
+           },
+           {
+            "paragraph":null,
+            "image":"/image/events/Vision_Quest/Vision_Quest_2.png"
+           },
+           {
+            "paragraph":"The event was a great success, with many students attending and learning a lot about software development and UI/UX design. The participants were enthusiastic, and the experts' presentations were engaging and informative. Overall, the event was an excellent opportunity for +2 students to learn about the IT industry and gain valuable insights into software development and UI/UX design.",
+            "image":null
+           }
         ],
         "youtubeCode": null,
         "images": []
@@ -59,7 +135,9 @@ var moreEvents = [
         "location": "Herald College, Naxal",
         "headerImage": "/image/events/Photoshop_Session_1.png",
         "profileImage": "/image/events/Photoshop_Session.png",
-        "description": ["UI Visuals successfully organized a session on Photoshop Session for beginners. The event provided an opportunity for participants to learn how to use Photoshop tools to edit and manipulate images, as well as design minimalistic posters. The session was beneficial for students who are starting their journey in design and editing, as well as those who want to become familiar with other Adobe software in the future. Participants were able to book their seats by filling out a form and joining the event. UI Visuals hopes that the knowledge and skills gained from this event will be useful for participants in their future design endeavors."],
+        "description": [
+            "UI Visuals successfully organized a session on Photoshop Session for beginners. The event provided an opportunity for participants to learn how to use Photoshop tools to edit and manipulate images, as well as design minimalistic posters. The session was beneficial for students who are starting their journey in design and editing, as well as those who want to become familiar with other Adobe software in the future. Participants were able to book their seats by filling out a form and joining the event. UI Visuals hopes that the knowledge and skills gained from this event will be useful for participants in their future design endeavors."
+        ],
         "youtubeCode": null,
         "images": []
     },
@@ -156,7 +234,7 @@ function seeMoreEvents(isMore) {
                 <h5 class="text-xxs">${moreEvents[i].date}</h5>
               </div>
               <h5 class="text-xs text-text-main min-h-[190px]">
-                ${moreEvents[i].description[0].substr(0, 220) + "..."}
+                ${moreEvents[i].description[0].paragraph.substr(0, 220) + "..."}
               </h5>
               <a class="mx-[auto] mt-[10px]" href=${"/event/" + moreEvents[i]?.id} target="_blank">
               <button class="bg-text-light hover:bg-text-mid duration-500 px-[30px] py-[10px] text-xxs text-primary-main">Read More</button>
